1e90746f3c2ad762aaefcfa9d5265923e86dc60f
marcellasasso <marcellasasso@gmail.com>
2023-09-20 18:01:24 -0300

    até aula executando fora do expo go

--- App.tsx
+++ App.tsx
@@ -2,12 +2,16 @@ import { StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
+import { OneSignal } from 'react-native-onesignal';
+
 import { Routes } from './src/routes';
 
 import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
+
+OneSignal.initialize('c08cc749-1a50-4277-9b99-eab7ef86e75d')
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
