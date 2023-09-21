1afe27c66eb3c4848b59340f87b321e57dfdb1a6
marcellasasso <marcellasasso@gmail.com>
2023-09-21 15:39:42 -0300

    até aula conhecendo mais o custom development client

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import { StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
-import { OneSignal } from 'react-native-onesignal';
+import { LogLevel, OneSignal } from 'react-native-onesignal';
 
 import { Routes } from './src/routes';
 
@@ -11,7 +11,11 @@ import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
 
+OneSignal.Debug.setLogLevel(LogLevel.Verbose);
+
 OneSignal.initialize('c08cc749-1a50-4277-9b99-eab7ef86e75d')
+
+OneSignal.Notifications.requestPermission(true);
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
